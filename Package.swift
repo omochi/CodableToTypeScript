04c4157e8b5a6f8b624bcde4db494f3c2942f64d
okamura <okamura@qoncept.co.jp>
2022-06-21 10:38:27 +0900

    update SwiftTypeReader version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.6
 
 import PackageDescription
 
@@ -11,7 +11,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/omochi/SwiftTypeReader", .branch("main"))
+        .package(url: "https://github.com/omochi/SwiftTypeReader", branch: "main"),
     ],
     targets: [
         .target(
