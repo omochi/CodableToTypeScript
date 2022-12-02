2c5f084d7540acebeec9ba569b2708500d4d9736
Iceman <okamura@qoncept.co.jp>
2022-12-02 14:51:35 +0900

    Update TypeScriptAST

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/omochi/SwiftTypeReader", from: "2.1.1"),
-        .package(url: "https://github.com/omochi/TypeScriptAST", from: "1.1.1")
+        .package(url: "https://github.com/omochi/TypeScriptAST", from: "1.2.0")
     ],
     targets: [
         .target(
