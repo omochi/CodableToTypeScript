565c489df1498387d1e1ac846fcbe1ad65093f54
omochimetaru <omochi.metaru@gmail.com>
2022-12-25 16:07:35 +0900

    internal extension

--- Sources/CodableToTypeScript/Extensions/TSASTEx.swift
+++ Sources/CodableToTypeScript/Extensions/TSASTEx.swift
@@ -1,14 +1,14 @@
 import TypeScriptAST
 
 extension TSObjectType.Field {
-    public static func field(
+    static func field(
         name: String, isOptional: Bool = false, type: any TSType
     ) -> TSObjectType.Field {
         let decl = TSFieldDecl(name: name, isOptional: isOptional, type: type)
         return .field(decl)
     }
 
-    public static func index(
+    static func index(
         name: String, index: any TSType, value: any TSType
     ) -> TSObjectType.Field {
         let decl = TSIndexDecl(name: name, index: index, value: value)
@@ -17,7 +17,7 @@ extension TSObjectType.Field {
 }
 
 extension TSObjectType {
-    public static func dictionary(_ value: any TSType) -> TSObjectType {
+    static func dictionary(_ value: any TSType) -> TSObjectType {
         return TSObjectType([
             .index(name: "key", index: TSIdentType.string, value: value)
         ])
