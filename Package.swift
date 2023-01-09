bcae4678623702ded092de31729102085a97bba0
omochimetaru <omochi.metaru@gmail.com>
2023-01-10 08:00:07 +0900

    update TSAST

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/omochi/SwiftTypeReader", from: "2.3.2"),
-        .package(url: "https://github.com/omochi/TypeScriptAST", from: "1.8.4")
+        .package(url: "https://github.com/omochi/TypeScriptAST", from: "1.8.5")
     ],
     targets: [
         .target(
