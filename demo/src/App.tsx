741e2b85c56cb2741d3aa169a134dc5def2193cf
Iceman <okamura@qoncept.co.jp>
2023-01-12 20:17:46 +0900

    editor area size fix

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -2,17 +2,22 @@ import reactLogo from './assets/react.svg'
 import { Editors } from './Editors';
 
 function App() {
-
   return (
     <div style={{
       display: "flex", flexDirection: "column",
+      width: "100vw", height: "100vh",
     }}>
-      <header> 
-        CodableToTypeScript
+      <header style={{ 
+        height: "3.2rem",
+        display: "flex", flexDirection: "row", alignItems: "center"
+      }}>
+        <h3 style={{ margin: "8pt" }}>
+          CodableToTypeScript
+        </h3>
       </header>
       <main style={{
+        flexGrow: 1,
         display: "flex",
-        width: "100vw", height: "100vh"
       }}>
         <Editors/>
       </main>
