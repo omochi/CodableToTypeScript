e1c173964bd090b05adde84a795ccb10fdedc2e0
omochimetaru <omochi.metaru@gmail.com>
2023-01-14 14:04:02 +0900

    release build

--- demo/ci/build.bash
+++ demo/ci/build.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -uexo pipefail
 
-swift build --product C2TS --triple wasm32-unknown-wasi
-wasm-opt -Os .build/debug/C2TS.wasm -o public/C2TS.wasm
+swift build --product C2TS --triple wasm32-unknown-wasi -c release
+wasm-opt -Os .build/release/C2TS.wasm -o public/C2TS.wasm
 wasm-strip public/C2TS.wasm
 
 npm install
