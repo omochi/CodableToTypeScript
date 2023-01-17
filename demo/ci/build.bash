b75c599073123b6399b61004d32222e40cc154cf
omochimetaru <omochi.metaru@gmail.com>
2023-01-17 17:54:09 +0900

    npm run swiftbuild

--- demo/ci/build.bash
+++ demo/ci/build.bash
@@ -2,9 +2,6 @@
 set -uexo pipefail
 cd "$(dirname "$0")/.."
 
-swift build --product C2TS --triple wasm32-unknown-wasi -c release
-wasm-opt -Os .build/release/C2TS.wasm -o public/C2TS.wasm
-wasm-strip public/C2TS.wasm
-
 npm install
+npm run swiftbuild
 npm run build
